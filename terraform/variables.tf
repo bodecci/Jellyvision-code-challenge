2e2c72f6df5712aabb08dceba2fcc32793b476e8
Bode F <bode.falade@outlook.com>
2024-10-17 14:19:53 -0500

    added variables to use.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+# AWS Configuration
 variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
@@ -16,6 +17,7 @@ variable "account_id" {
   default     = "933071741192"
 }
 
+# ECS Configuration
 variable "ecs_cluster_name" {
   description = "Name of the ECS Cluster"
   type        = string
@@ -43,15 +45,44 @@ variable "container_name" {
 variable "image_tag" {
   description = "Docker image tag for ECR"
   type        = string
-  default     = "latest"
+  default     = "1.3"
 }
 
-variable "subnets" {
-  description = "Subnets for ECS Service"
-  type        = list(string)
+# ACM Certificate for HTTPS
+variable "certificate_arn" {
+  description = "ARN of the ACM Certificate for the domain"
+  type        = string
+  default     = "arn:aws:acm:us-east-1:933071741192:certificate/abcd1234-56ef-78gh-90ij-klmnopqrstuv"
 }
 
-variable "security_groups" {
-  description = "Security groups for ECS tasks"
+# Network Configuration
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+# Security Groups
+variable "alb_sg_description" {
+  description = "Description of the security group for the ALB"
+  type        = string
+  default     = "Security group for the Application Load Balancer (ALB)"
+}
+
+variable "ecs_sg_description" {
+  description = "Description of the security group for ECS tasks"
+  type        = string
+  default     = "Security group for ECS tasks in the private subnets"
 }
